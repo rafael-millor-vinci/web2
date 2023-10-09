ebfa8d0945edb462f7556c7e7929831c890ce9f6
rafael-millor-vinci <rafael.millor@student.vinci.be>
2023-10-09 11:17:29 +0200

    2.1 : HMI : first external script.

---
+++ exercises/2.1/external-scripts/external-script.js
@@ -0,0 +1,6 @@
+
+const dateTimeNow = new Date();
+console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
+console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
+
+alert(dateTimeNow.toLocaleDateString()+" "+ dateTimeNow.toLocaleTimeString() + " : This is the best moment to have a look at this website !");
