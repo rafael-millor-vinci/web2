c2e7077dd730a4b75562d4a0f1328276f66f6622
rafael-millor-vinci <rafael.millor@student.vinci.be>
2023-11-14 14:17:14 +0100

    2.15 : view online jokes

---
+++ exercises/2.15/js-router-boilerplate/src/Components/Pages/HomePage.js
@@ -0,0 +1,20 @@
+const HomePage = () => {
+  const main = document.querySelector('main');
+  main.innerHTML = 'Deal with the content of your HomePage';
+
+  fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+  .then((response) => {
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+    return response.json();
+  })
+  .then((joke) => {
+    main.innerHTML = `<div> Joke categ : ${joke.category} </div> `;
+    main.innerHTML += `<div>joke : ${joke.joke}</div> `;
+    
+  })
+  .catch((err) => {
+    console.log('HOMEpage error : ',err);
+  });
+};
+
+export default HomePage;
